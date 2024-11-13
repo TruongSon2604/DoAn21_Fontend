98b59c767ce221a86ecb0e60ce762565da197322
TruongSon2604 <truongngocson26042003@gmail.com>
2024-11-13 16:37:03 +0700

    done header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import MapComponent from "./component/MapComponent";
+
 import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
+import "./scssFolder/base/_reset.scss";
+import "./scssFolder/base/_base.scss";
+
+// import "./scssFolder/main.scss";
+
 import Home from "./pages/Home/Home";
-import Demo from "./pages/Demo/Demo";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+
 function App() {
   return (
     <>
       <div className="app">
         <Routes>
           <Route path="/" element={<Home />} />
-        
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </div>
     </>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,116 +1,18 @@
 import React from "react";
-// import "./Grid.css";
+
 import "./Home.css";
+import Header from "../../component/header/Header";
+import Footer from "../../component/footer/Footer";
+
+import "../../scssFolder/base/_base.scss";
+import "../../scssFolder/base/_reset.scss";
 const Home = () => {
   return (
     <>
-      {" "}
-      <div className="container">
-     
-        <header className="header">
-          <div className="inner">Header</div>
-        </header>
-       
-        <section>
-          <div className="row">
-            <div className="col-lg-6 col-md-6">
-              <div className="inner">Herro left</div>
-            </div>
-            <div className="col-lg-5 offset-lg-1 col-md-6">
-              <div className="inner">Herro right</div>
-            </div>
-          </div>
-        </section>
-        {/* section2*/}
-        <section>
-          <div className="row">
-            <div className="col-lg-5 col-md-6">
-              <div className="inner"> left</div>
-            </div>
-            <div className="col-lg-5 offset-lg-2 col-md-6">
-              <div className="inner"> right</div>
-            </div>
-          </div>
-        </section>
-        {/* section3*/}
-        <section>
-          <div className="row">
-            <div className="col-lg-5 col-md-6">
-              <div className="inner"> left</div>
-            </div>
-            <div className="col-lg-6 offset-lg-1 col-md-6">
-              <div className="inner"> right</div>
-            </div>
-          </div>
-        </section>
-        {/* section4*/}
-        <section>
-          <div className="row">
-            <div className="col-lg-6 col-md-6">
-              <div className="inner"> left</div>
-            </div>
-            <div className="col-lg-5 offset-lg-1 col-md-6">
-              <div className="inner"> right</div>
-            </div>
-          </div>
-        </section>
-        {/* section5*/}
-        <section>
-          <div className="row">
-            <div className="col">
-              <div className="inner">Center</div>
-            </div>
-          </div>
-          <div className="row row-cols-md-2 row-cols-1">
-            <div className="col">
-              <div className="inner">item1</div>
-            </div>
-            <div className="col">
-              <div className="inner">item2</div>
-            </div>
-          </div>
-        </section>
-        {/* section6*/}
-        <section>
-          <div className="row">
-            <div className="col">
-              <div className="inner">
-                <div>left</div>
-                <div>right</div>
-              </div>
-            </div>
-          </div>
-          <div className="row row-cols-lg-2 row-cols-1">
-            <div className="col">
-              <div className="inner">item1</div>
-            </div>
-            <div className="col">
-              <div className="inner">item2</div>
-            </div>
-            <div className="col">
-              <div className="inner">item3</div>
-            </div>
-            <div className="col">
-              <div className="inner">item4</div>
-            </div>
-          </div>
-        </section>
-        <section>
-          <div className="row">
-            <div className="col">
-              <div className="inner">Dess</div>
-            </div>
-          </div>
-        </section>
-      </div>
-      {/* // End container */}
-      <footer>
-        <div className="container">
-          <div className="inner">Footer</div>
-        </div>
-      </footer>
+      <Header />
+      <div className="container">Content Home</div>
+      <Footer />
     </>
   );
 };
-
 export default Home;
